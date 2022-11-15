30a8d8c54c62378be7f944a1cda179a771e69337
Shuhad786 <shuhad.loofer@gmail.com>
2022-11-15 20:17:23 +0200

    correct linter errors

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,31 +1,31 @@
 import React, { useState } from 'react';
-import { addBook } from '../redux/books/books';
 import { useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuidv4';
+import { addBook } from '../redux/books/books';
 
 const AddBook = () => {
-  const [dataTitle, setTitle] = useState('')
-  const [dataAuthor, setAuthor] = useState('')
+  const [dataTitle, setTitle] = useState('');
+  const [dataAuthor, setAuthor] = useState('');
 
   const dispatch = useDispatch();
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newBook = {id: uuidv4(), title: dataTitle, author: dataAuthor}
+    const newBook = { id: uuidv4(), title: dataTitle, author: dataAuthor }
     setTitle('');
     setAuthor('');
     dispatch(addBook(newBook));
   };
 
   return (
-    <div className='formContainer' >
-      <form className='form' onSubmit={handleSubmit}>
+    <div className="formContainer" >
+      <form className="form" onSubmit={handleSubmit}>
         <label>Add a new book</label>
-        <input className='input' onChange={(e) => setTitle(e.target.value)} value={dataTitle} type='text' placeholder='Enter book title'/>
-        <input className='input' onChange={(e) => setAuthor(e.target.value)} value={dataAuthor} type='text' placeholder='Enter the authors name'/>
-        <button type='submit' className='submitBtn'>Submit</button>
+        <input className="input" onChange={(e) => setTitle(e.target.value)} value={ dataTitle } type="text" placeholder="Enter book title"/>
+        <input className="input" onChange={(e) => setAuthor(e.target.value)} value={ dataAuthor } type="text" placeholder="Enter the authors name"/>
+        <button type="submit" className="submitBtn">Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default AddBook;
